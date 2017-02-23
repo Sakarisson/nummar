050e973ee4382e4006f66bf5fb085b2e9a6f2338
Kristian Sakarisson <kristian@sakarisson.com>
2017-02-23 13:34:44 +0000

    Changed app to fetch data from in-page script

--- app.js
+++ app.js
@@ -1,7 +1,15 @@
-const request = require('request')
-const async = require('async')
-const cheerio = require('cheerio')
+const request = require('request')   // For making the actual requests to the servers
+const async = require('async')       // To avoid callback hell
+const cheerio = require('cheerio')   // To get data from DOM
+const _ = require('underscore')      // Because I'm lazy
 
+/**
+ * Get info from nummar.fo from a certain parameter string
+ *
+ * Input:  string
+ *
+ * Return: callback (either an error or the body of the page)
+ */
 function getInfo (params, callback) {
   const url = `http://nummar.fo/?s=${params}`
   request(url, (error, response, body) => {
@@ -9,7 +17,7 @@ function getInfo (params, callback) {
       if (response.statusCode === 200) {
         callback(null, body)
       } else {
-        const errorMsg = `Invalid status code: ${response.statusCode}`
+        const errorMsg = new Error(`Invalid status code: ${response.statusCode}`)
         callback(errorMsg)
       }
     } else {
@@ -18,42 +26,35 @@ function getInfo (params, callback) {
   })
 }
 
-function getResults (body, callback) {
-  const htmlResults = []
-  const results = []
-  let $ = cheerio.load(body)
-  $('.mdCardPerson').each(function () {
-    htmlResults.push($(this).html())
-  })
-  htmlResults.forEach((r) => {
-    const newResult = {}
-    let $ = cheerio.load(r)
-    newResult.name = $('.fn').text()
-    newResult.streetAddress = $('.street-address').text()
-    newResult.postalCode = $('.postal-code').text()
-    newResult.locality = $('.locality').text()
-    newResult.region = $('.region*').text()
-    newResult.numbers = {}
-    $('.tel').each(function (i, e) {
-      let $ = cheerio.load(e)
-      const type = $('.type').text()
-      if (type === 'Fartelefon') {
-        newResult.numbers.mobile = $('.value').text()
-      } else if (type === 'Fastnet') {
-        newResult.numbers.fastnet = $('.value').text()
-      }
-    })
+/**
+ * Parse the page body from getInfo() and get JSON data for results
+ *
+ * Input:  string (body HTML)
+ *
+ * Return: callback (JSON object)
+ */
+function getJSON (body, callback) {
+  const $ = cheerio.load(body) // Parse body
+  const scripts = $('script').filter(() => true) // Split all scripts into an array
+  const jsonScript = _.filter(scripts, (s) => cheerio.load(s).html().includes('var json_body'))[0] // Get the first (only) element, which contains the words 'var json_body'
 
-    results.push(newResult)
-  })
-  callback(null, results)
+  const jsonBody = JSON.parse(cheerio.load(jsonScript).text().replace('var json_body = ', '')) // Parse everything after the variable declaration to JSON
+
+  callback(null, jsonBody)
 }
 
+/**
+ * Search function. Runs an async-waterfall and returns either an error or a result
+ *
+ * Input:  string (search parameters)
+ *
+ * Return: callback (either an error or an array of JSON objects)
+ */
 exports.search = (param, callback) => {
   async.waterfall([
-    (callback) => { callback(null, param) },
+    (callback) => { callback(null, param) }, // Anonymous callback function to pass parameters
     getInfo,
-    getResults
+    getJSON
   ], (error, result) => {
     callback(error, result)
   })
