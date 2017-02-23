df28ff0f1482e5bd517f87d90202cbf8f5db5f14
Kristian Sakarisson <kristian@sakarisson.com>
2017-02-23 11:08:16 +0000

    Forgot to add phone number.. doh!

--- app.js
+++ app.js
@@ -4,7 +4,6 @@ const cheerio = require('cheerio')
 
 function getInfo (params, callback) {
   const url = `http://nummar.fo/?s=${params}`
-  console.log(url)
   request(url, (error, response, body) => {
     if (!error) {
       if (response.statusCode === 200) {
@@ -24,16 +23,26 @@ function getResults (body, callback) {
   const results = []
   let $ = cheerio.load(body)
   $('.mdCardPerson').each(function () {
-    htmlResults.push({ html: $(this).html() })
+    htmlResults.push($(this).html())
   })
   htmlResults.forEach((r) => {
     const newResult = {}
-    let $ = cheerio.load(r.html)
+    let $ = cheerio.load(r)
     newResult.name = $('.fn').text()
     newResult.streetAddress = $('.street-address').text()
     newResult.postalCode = $('.postal-code').text()
     newResult.locality = $('.locality').text()
     newResult.region = $('.region*').text()
+    newResult.numbers = {}
+    $('.tel').each(function (i, e) {
+      let $ = cheerio.load(e)
+      const type = $('.type').text()
+      if (type === 'Fartelefon') {
+        newResult.numbers.mobile = $('.value').text()
+      } else if (type === 'Fastnet') {
+        newResult.numbers.fastnet = $('.value').text()
+      }
+    })
 
     results.push(newResult)
   })
